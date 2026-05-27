7e83f0b6fbb3a94f9c86433188e1ec1963718d60
janicelav9 <unicorntanyongnie@gmail.com>
2026-05-27 21:58:55 +0800

    fix: remove unused PutObjectCommand import

--- src/main/java/com/example/demo/ServiceFormController.java
+++ src/main/java/com/example/demo/ServiceFormController.java
@@ -28,7 +28,6 @@ import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.PutObjectCommand;
 
 @RestController
 @CrossOrigin(origins = "*")
